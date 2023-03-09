390d3f8b957bc76277d93c7ed85601838dbdf8ee
tsizip <tsizip@gmail.com>
2023-03-09 14:30:20 +0700

    API chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Chatbox from './component/Chatbox';
+// import Chatbox from './component/Chatbox';
 import Boxchat from './component/Boxchat';
 
 function App() {
