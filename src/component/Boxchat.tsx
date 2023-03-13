330df17c9b3284a776e64734ceb669621f94a1b2
tsizip <tsizip@gmail.com>
2023-03-13 09:44:09 +0700

    validation_prompt

--- src/component/Boxchat.tsx
+++ src/component/Boxchat.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import { OpenAIService } from '../service/config'
 import moment from 'moment'
 
+
 type Props = {}
 
 export default function Boxchat({ }: Props) {
@@ -14,9 +15,15 @@ export default function Boxchat({ }: Props) {
      const [click, setClick] = useState(true)
      const [prompt, setPrompt] = useState('')
      const [logChat, setLogChat] = useState([
-          { type: 'AI', mess: 'Hi, can I help you?' },
+          { type: 'AI', mess: 'Xin chào, tôi có thể giúp gì cho bạn?' },
      ])
 
+     const [logChatOld, setLogChatOld] = useState([
+          { type: 'AI', mess: 'Xin chào, tôi có thể giúp gì cho bạn?' },
+     ])
+
+    
+     const [typing, setTyping] = useState(false)
      const [QAinit, setQAinit] = useState(String)
 
      let img1 = require('../asset/images/chatbot.png')
@@ -32,6 +39,7 @@ export default function Boxchat({ }: Props) {
 
      const xuly = async () => {
           let value = valueFinal + final
+          // console.log('data prompt', value)
           if (count <= 1) {
                callApi(value)
           } else {
@@ -40,35 +48,49 @@ export default function Boxchat({ }: Props) {
 
      }
 
-     const callApi = OpenAIService.callApi(QAinit, setQAinit, valueFinal, setFinal, setLogChat, logChat)
+     const callApi = OpenAIService.callApi(QAinit, setTyping, typing, setQAinit, valueFinal, setFinal, setLogChat, logChat, setPrompt, prompt, logChatOld, setLogChatOld)
 
      useEffect(() => {
           executeScroll()
           localStorage.setItem('data', final)
-          console.log('final', final)
+          // console.log('logChat', logChat)
+          console.log('data prompt', final)
+
      }, [final])
+
+
+     // test include
+     // let text = "' ' § Code # 5 t = '' for x in t: print(x) § Output > stdout : ['p\n', 'y\n', 't\n', 'h\n', 'o\n', 'n\n'] § Code #6 t = (1,2,3,4,5) for num in t: print(num) § Output > stdout : ['1\n', '2\n', '3\n', '4\n', '5\n'] § Code #7 l = [1, 2, 3, 4, 5] for num in l: num += 1 print(num) § Output > stdout : ['2\n', '3\n', '4\n', '5\n', '6\n'] § Code #8 d = {'x':1, 'y':2, 'z':3} for key, value in d.items(): print(key, ) § Output > stdout :  § Code #9 l = [1, 2, 3, 4, 5] total = 0 for num in l: total += num print(total) § Output > stdout : ['15\n'] § Code #10 d = {'x':1, 'y':2, 'z':3} for key in d: print(key) § Output > stdout : ['x\n', 'y\n', 'z\n'] § Code § END OF DOC"
+     // console.log(text.includes('§'))
+     // console.log()
 
      return (
           <div>
                <div className='parent'>
-                    <div className="chat-Box">
+                    <div className="chat-Box" style={{ width: !click ? '225px' : '300px'}}>
                          {/* --header-- */}
                          <div className='header'>
                               <div className="header_chat-Box">
-                                   <div className="header_chat-Box-left" onClick={hiddenBox}>
+                                   <div className="header_chat-Box-left" style={{justifyContent: click ? 'unset' : 'center'}} onClick={hiddenBox}>
                                         <img alt='img1' className="content_chat-BoxImg" src={img1} />
                                         <div className="header_chat-BoxText">
                                              <p className='header_nameBot'>Mekomed Chatbot</p>
                                              <p className='header_statusBot'><i className="fa fa-dot-circle"></i> Online now</p>
                                         </div>
+                                        {/* <iframe src="https://embed.lottiefiles.com/animation/12966"></iframe> */}
+
                                    </div>
-                                   <div className="header_chat-Box-right footer_chat-Box-hover" onClick={hiddenBox}>
+                                   <div style={{display: click ? 'block' : 'none'}} className="header_chat-Box-right footer_chat-Box-hover" onClick={hiddenBox}>
                                         {click ? <i className="fas fa-minus" /> : <i className="fas fa-plus" />}
                                    </div>
                               </div>
                          </div>
                          {/* --content-- */}
                          <div style={{ display: click ? 'block' : 'none' }} ref={myRef} className="content_chat-Box">
+
+
+                              {/* <img src="./typingchat.gif" ></img>  */}
+
                               <p className='dateTime'>{moment(Date.now()).format('hh:mm A')}</p>
                               {logChat?.map((value, index) => {
                                    if (value.type === 'Human') {
@@ -94,6 +116,7 @@ export default function Boxchat({ }: Props) {
                          </div>
                          {/* --footer-- */}
                          <div className='ft' style={{ display: click ? 'block' : 'none' }}>
+                              {typing ? <img src='https://raw.githubusercontent.com/reachtokish/typing-animation/master/giphy.gif' className='typing_load' /> : null}
                               <div className='ft_bot'>
                                    <div className="footer_chat-Box">
                                         {/* <div className="footer_chat-Box-hover">
@@ -102,18 +125,29 @@ export default function Boxchat({ }: Props) {
 
                                         <form onSubmit={async function (e) {
                                              e.preventDefault()
+                                             await setTyping(true)
                                              await setCount(count + 1)
                                              let arr = await logChat
-                                             await arr.push({ type: 'Human', mess: prompt })
 
-                                             await setLogChat(arr)
-                                             let promptValue = `${prompt}\\n`
-                                             console.log('submit', promptValue)
-                                             await setQAinit(promptValue)
-                                             valueFinal = valueFinal + promptValue
-                                             await setFinal(valueFinal + final)
-                                             await xuly()
-                                             await setPrompt('')
+                                             let checkSpam = await prompt.includes(' ')
+                                             if (checkSpam) {
+                                                  await arr.push({ type: 'Human', mess: prompt })
+
+                                                  await setLogChat(arr)
+                                                  let promptValue = `${prompt}\\n`
+                                                  console.log('submit', promptValue)
+                                                  await setQAinit(promptValue)
+                                                  valueFinal = valueFinal + promptValue
+                                                  await setFinal(valueFinal + final)
+                                                  await xuly()
+                                                  await setPrompt('')
+                                             } else {
+                                                  alert('vui long nhap du lieu hop le')
+                                                  await setTyping(false)
+                                                  await setPrompt('')
+                                             }
+
+
 
                                         }} className="footer_chat-Box-input">
                                              <input id='inputChat' value={prompt} type="text" placeholder="Type a Message..." className="footer-text" onChange={(e) => {
@@ -121,21 +155,43 @@ export default function Boxchat({ }: Props) {
                                              }} />
                                         </form>
                                         <div className="footer_chat-Box-hover" onClick={async function () {
+                                             // await setCount(count + 1)
+                                             // let arr = await logChat
+                                             // await arr.push({ type: 'Human', mess: prompt })
+
+                                             // await setLogChat(arr)
+                                             // let promptValue = `${prompt}\\n`
+                                             // console.log('submit', promptValue)
+                                             // await setQAinit(promptValue)
+                                             // valueFinal = valueFinal + promptValue
+                                             // await setFinal(valueFinal + final)
+
+                                             // await xuly()
+                                             // await setPrompt('')
+                                             await setTyping(true)
                                              await setCount(count + 1)
                                              let arr = await logChat
-                                             await arr.push({ type: 'Human', mess: prompt })
 
-                                             await setLogChat(arr)
-                                             let promptValue = `${prompt}\\n`
-                                             console.log('submit', promptValue)
-                                             await setQAinit(promptValue)
-                                             valueFinal = valueFinal + promptValue
-                                             await setFinal(valueFinal + final)
-                                             await xuly()
-                                             await setPrompt('')
+                                             let checkSpam = await prompt.includes(' ')
+                                             if (checkSpam) {
+                                                  await arr.push({ type: 'Human', mess: prompt })
+
+                                                  await setLogChat(arr)
+                                                  let promptValue = `${prompt}\\n`
+                                                  console.log('submit', promptValue)
+                                                  await setQAinit(promptValue)
+                                                  valueFinal = valueFinal + promptValue
+                                                  await setFinal(valueFinal + final)
+                                                  await xuly()
+                                                  await setPrompt('')
+                                             } else {
+                                                  alert('vui long nhap du lieu hop le')
+                                                  await setTyping(false)
+                                                  await setPrompt('')
+                                             }
 
                                         }}>
-                                             <svg className="fill" fill='#2D87F7' viewBox="0 0 24 24" width="20px"><path d="M16.6915026,12.4744748 L3.50612381,13.2599618 C3.19218622,13.2599618 3.03521743,13.4170592 3.03521743,13.5741566 L1.15159189,20.0151496 C0.8376543,20.8006365 0.99,21.89 1.77946707,22.52 C2.41,22.99 3.50612381,23.1 4.13399899,22.8429026 L21.714504,14.0454487 C22.6563168,13.5741566 23.1272231,12.6315722 22.9702544,11.6889879 C22.8132856,11.0605983 22.3423792,10.4322088 21.714504,10.118014 L4.13399899,1.16346272 C3.34915502,0.9 2.40734225,1.00636533 1.77946707,1.4776575 C0.994623095,2.10604706 0.8376543,3.0486314 1.15159189,3.99121575 L3.03521743,10.4322088 C3.03521743,10.5893061 3.34915502,10.7464035 3.50612381,10.7464035 L16.6915026,11.5318905 C16.6915026,11.5318905 17.1624089,11.5318905 17.1624089,12.0031827 C17.1624089,12.4744748 16.6915026,12.4744748 16.6915026,12.4744748 Z" /></svg>
+                                             <svg className="fill" fill='#008000' viewBox="0 0 24 24" width="20px"><path d="M16.6915026,12.4744748 L3.50612381,13.2599618 C3.19218622,13.2599618 3.03521743,13.4170592 3.03521743,13.5741566 L1.15159189,20.0151496 C0.8376543,20.8006365 0.99,21.89 1.77946707,22.52 C2.41,22.99 3.50612381,23.1 4.13399899,22.8429026 L21.714504,14.0454487 C22.6563168,13.5741566 23.1272231,12.6315722 22.9702544,11.6889879 C22.8132856,11.0605983 22.3423792,10.4322088 21.714504,10.118014 L4.13399899,1.16346272 C3.34915502,0.9 2.40734225,1.00636533 1.77946707,1.4776575 C0.994623095,2.10604706 0.8376543,3.0486314 1.15159189,3.99121575 L3.03521743,10.4322088 C3.03521743,10.5893061 3.34915502,10.7464035 3.50612381,10.7464035 L16.6915026,11.5318905 C16.6915026,11.5318905 17.1624089,11.5318905 17.1624089,12.0031827 C17.1624089,12.4744748 16.6915026,12.4744748 16.6915026,12.4744748 Z" /></svg>
                                         </div>
                                    </div>
                               </div>
--- src/service/config.tsx
+++ src/service/config.tsx
@@ -1,8 +1,10 @@
+import { includes } from "lodash";
 
 
 export const OpenAIService = {
-     callApi: (QAinit:string,setQAinit:any,valueFinal:string, setFinal:any, setLogChat:any,logChat:any )=>{
+     callApi: (QAinit: string, setTyping: any, typing: any, setQAinit: any, valueFinal: string, setFinal: any, setLogChat: any, logChat: any, setPrompt: any, prompt: any, logChatOld: any, setLogChatOld: any) => {
           return async (text: string | any) => {
+               let spam = 0
                const { Configuration, OpenAIApi } = require("openai");
                const configuration = new Configuration({
                     apiKey: `${process.env.REACT_APP_SECRET_NAME}`,
@@ -11,25 +13,41 @@ export const OpenAIService = {
                const response = await openai.createCompletion({
                     model: "text-davinci-003",
                     prompt: text,
-                    temperature: 0.5,
+                    temperature: 0.9,
                     max_tokens: 3000,
                     top_p: 1,
                     frequency_penalty: 0,
                     presence_penalty: 0.6,
                     stop: [" Human:", " AI:"],
                });
-     
-               if(response.data.choices[0].text !== ''){
+               let res = await response.data.choices[0].text
+
+               // console.log('res',res)
+               if (res.includes('§') !== true && res.includes('END OF DOC') !== true && res.includes('str.format') !== true && res.includes('#') !== true && res.includes('\\n') !== true && res.includes('stdout') !== true) {
                     let newChat = `${response.data.choices[0].text}\\n`
                     let value = QAinit + newChat
                     await setQAinit(value)
                     valueFinal = valueFinal + newChat
                     await setFinal(valueFinal)
-          
+                    // await setLogChatOld([...logChat])
                     await setLogChat([...logChat, { type: 'AI', mess: response.data.choices[0].text }])
+                    await setTyping(false)
+                    // await console.log(logChat)
                } else {
+                    spam++
+                    // alert('vui long nhap du lieu hop le')
+                    if (spam >= 5) {
+                         await setLogChat([...logChat, { type: 'AI', mess: 'Tôi không hiểu bạn đang nói gì. Vui lòng gửi lại 1 câu hỏi khác!' }])
+                         // await setLogChat([{ type: 'AI', mess: 'Xin chào, tôi có thể giúp gì cho bạn?' }, { type: 'Human', mess: prompt }, , { type: 'AI', mess: 'Tôi không hiểu bạn đang nói gì. Vui lòng gửi lại 1 câu hỏi khác!' }])
+                         await setLogChatOld(logChat)
+                         await setPrompt('')
+                         await setTyping(false)
+                    } else {
+                         await setLogChat([...logChat, { type: 'AI', mess: 'Tôi không hiểu bạn đang nói gì. Vui lòng gửi lại 1 câu hỏi khác!' }])
+                         await setPrompt('')
+                         await setTyping(false)
+                    }
 
-                    await setLogChat([...logChat, { type: 'AI', mess: 'Please describe your question clearly!' }])
                }
           }
      }
